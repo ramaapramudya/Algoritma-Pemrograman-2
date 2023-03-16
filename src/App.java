a459f53b84886fdb4b8d71ae78f209643849420d
ramaapramudya <ramaapramudya@gmail.com>
2023-03-16 21:34:10 +0700

    Tugas Pertemuan 3 Final

--- src/App.java
+++ src/App.java
@@ -8,6 +8,26 @@ public class App {
         System.out.print("NPM \t: 2022320019\n");
         System.out.print("Kelas \t: FI22B\n");
 
+        double LuasLingkaran, LuasPersegiPanjang, phi=3.14;
+        int r, p, l;
+
+        System.out.print("\n(LINGKARAN) Masukkan jari-jari lingkaran : ");
+        r = input.nextInt();
+
+        System.out.print("\n(PERSEGI PANJANG) Masukkan panjang persegi panjang : ");
+        p = input.nextInt();
+        System.out.print("(PERSEGI PANJANG) Masukkan lebar persegi panjang : ");
+        l = input.nextInt();
+
+        LuasPersegiPanjang = p*l;
+        LuasLingkaran = phi*r*r;
+
+        System.out.println("\n======================================================");
+        System.out.println("PROGRAM PERHITUNGAN LUAS LINGKARAN DAN PERSEGI PANJANG");
+        System.out.println("======================================================");
+        System.out.println("Luas lingkaran adalah : " + LuasLingkaran);
+        System.out.println("Luas persegi panjang adalah : " + LuasPersegiPanjang);
+
         input.close();
     }
 }
